544f338892aacd9a7d8c0f49288eb2bad7dcc147
Hubert Bajon <s12699@pjwstk.edu.pl>
2026-03-17 21:20:44 +0100

    average function added

--- Very_first_project/Very_first_project/Program.cs
+++ Very_first_project/Very_first_project/Program.cs
@@ -1,1 +1,15 @@
 ﻿Console.WriteLine("My very first commit");
+
+
+static int average(int[] values)
+{
+    int sum = 0;
+
+    foreach (int i in values)
+    {
+        sum += i;
+    }
+    
+    int avg = sum / values.Length;
+    return avg;
+}
